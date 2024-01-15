5a950c38e27648ca2ae6e8cbc4782287e96d8269
Gunichi <gunichi297@gmail.com>
2024-01-15 11:25:27 -0300

    cover image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,100 +1,18 @@
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-import { useEffect, useMemo, useRef, useState } from "react";
-import Player from "video.js/dist/types/player";
 
 import { useTheme } from "@mui/material";
-import useOffSetTop from "src/hooks/useOffSetTop";
-import { useDetailModal } from "src/providers/DetailModalProvider";
-import {
-  useGetVideosByMediaTypeAndCustomGenreQuery,
-  useLazyGetAppendedVideosQuery,
-} from "src/store/slices/discover";
+
 import { MEDIA_TYPE } from "src/types/Common";
-import { Movie } from "src/types/Movie";
-import { getRandomNumber } from "src/utils/common";
-import MaturityRate from "./MaturityRate";
-import MaxLineTypography from "./MaxLineTypography";
-import MoreInfoButton from "./MoreInfoButton";
-import PlayButton from "./PlayButton";
 
 interface TopTrailerProps {
   mediaType: MEDIA_TYPE;
 }
 
 export default function TopTrailer({ mediaType }: TopTrailerProps) {
-  const data2 = {
-    results: [
-      {
-        "adult": false,
-        "backdrop_path": "/pA3vdhadJPxF5GA1uo8OPTiNQDT.jpg",
-        "genre_ids": [
-          28,
-          18
-        ],
-        "id": 678512,
-        "original_language": "en",
-        "original_title": "Slow Food",
-        "overview": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-        "popularity": 263.457,
-        "poster_path": "/qA5kPYZA7FkVvqcEfJRoOy4kpHg.jpg",
-        "release_date": "2023-07-03",
-        "title": "Slow Food",
-        "video": false,
-        "vote_average": 8.1,
-        "vote_count": 1585,
-        "age": '14+'
-      },
-    ]
-  }
 
   const theme = useTheme();
   const isDarkMode = theme.palette.mode === 'dark';
-  const { data } = useGetVideosByMediaTypeAndCustomGenreQuery({
-    mediaType,
-    apiString: "popular",
-    page: 1,
-  });
-  const [getVideoDetail, { data: detail }] = useLazyGetAppendedVideosQuery();
-  const [video, setVideo] = useState<Movie | null>(null);
-  const [muted, setMuted] = useState(true);
-  const playerRef = useRef<Player | null>(null);
-  const isOffset = useOffSetTop(window.innerWidth * 0.5625);
-  const { setDetailType } = useDetailModal();
-  const maturityRate = useMemo(() => {
-    return getRandomNumber(20);
-  }, []);
-
-  useEffect(() => {
-    if (playerRef.current) {
-      if (isOffset) {
-        playerRef.current.pause();
-      } else {
-        if (playerRef.current.paused()) {
-          playerRef.current.play();
-        }
-      }
-    }
-  }, [isOffset]);
-
-  useEffect(() => {
-    if (data2 && data2.results) {
-      const videos = data2.results.filter((item) => !!item.backdrop_path);
-      const selectedVideo = videos[getRandomNumber(videos.length)];
-  
-      // Ajuste o tipo de 'id' conforme necessário (convertendo para string, se necessário)
-      const adjustedVideo: Movie = { ...selectedVideo, id: String(selectedVideo.id) };
-  
-      setVideo(adjustedVideo);
-    }
-  }, [data2]);
-
-  useEffect(() => {
-    if (video) {
-      getVideoDetail({ mediaType, id: video.id });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [video]);
 
   return (
     <Box sx={{ position: "relative", zIndex: 1 }}>
@@ -115,7 +33,6 @@ export default function TopTrailer({ mediaType }: TopTrailerProps) {
             position: "absolute",
           }}
         >
-          {video && (
             <>
               <Box
                 sx={{
@@ -126,14 +43,12 @@ export default function TopTrailer({ mediaType }: TopTrailerProps) {
                   position: "absolute",
                 }}
               >
-                {detail && (
                   <img
-                    src='https://i.imgur.com/TrPiloH.png'
+                    src='./assets/slowfood.png'
                     alt={'Slow Food'}
                     width="100%"
                     height="100%"
                   />
-                )}
                 <Box
                   sx={{
                     // background: `linear-gradient(77deg,rgba(0,0,0,.6),transparent 85%)`,
@@ -233,7 +148,6 @@ export default function TopTrailer({ mediaType }: TopTrailerProps) {
                 </Stack> */}
               </Box>
             </>
-          )}
         </Box>
       </Box>
     </Box>
