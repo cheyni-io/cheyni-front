467f6f3ac87abac321ad3dfef3b5ecbdecbb0d79
Gunichi <gunichi297@gmail.com>
2024-03-01 14:56:20 -0300

    fix: footer size

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -10,18 +10,18 @@ export default function Footer() {
       <Box
         sx={{
           mb: 3,
-          pb: "40%",
+          pb: "30%", // Adjust this value to control the overall height of the footer
           top: 0,
           left: 0,
           right: 0,
           position: "relative",
-          height: "50%"
+          height: "50%",
         }}
       >
         <Box
           sx={{
             width: "100%",
-            height: "56.25vw",
+            height: "100%", // Set the height to "100%" to cover the entire parent Box
             position: "absolute",
           }}
         >
@@ -35,34 +35,14 @@ export default function Footer() {
                 position: "absolute",
               }}
             >
-              {/* <img
-                    src='./assets/slowfood.png'
-                    alt={'Slow Food'}
-                    width="100%"
-                    height="100%"
-                  /> */}
               <video
                 src="./assets/footerVideo.mp4"
                 autoPlay
                 muted
                 loop
                 playsInline
-                style={{ width: "100%", height: "100%" }}
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
               />
-              {/* <Box
-                sx={{
-                  // background: `linear-gradient(77deg,rgba(0,0,0,.6),transparent 85%)`,
-                  background: isDarkMode
-                    ? "linear-gradient(77deg,rgba(0,0,0,.6),transparent 85%)"
-                    : "linear-gradient(77deg,rgba(255,255,255,.6),transparent 100%)",
-                  top: 0,
-                  left: 0,
-                  bottom: 0,
-                  right: "26.09%",
-                  opacity: 1,
-                  position: "absolute",
-                }}
-              /> */}
               <Box
                 sx={{
                   top: 0,
@@ -82,8 +62,8 @@ export default function Footer() {
                   right: 0,
                   bottom: 0,
                   position: "absolute",
-                  backgroundColor: "#000", // Black background color
-                  opacity: "65%", // Adjust opacity as needed
+                  backgroundColor: "#000",
+                  opacity: "65%",
                 }}
               />
               <Box
@@ -96,46 +76,14 @@ export default function Footer() {
                   textAlign: "center",
                 }}
               >
-                {/* <Stack
-                  direction="row"
-                  spacing={1}
-                  sx={{ alignItems: "center" }}
-                >
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      flexDirection: "row",
-                    }}
-                  >
-                    <img
-                      src="/assets/logo-light.png"
-                      alt="Cheyni"
-                      style={{ width: "40%", marginRight: 2 }}
-                    />
-                    <Divider
-                      orientation="vertical"
-                      variant="middle"
-                      flexItem
-                      sx={{
-                        borderWidth: 1,
-                        borderColor: "white",
-                        mr: 2,
-                        ml: 2,
-                      }}
-                    />
-                    <Typography color="#FFF">
-                      UNLOCK LOYALTY WITH <b>STREAMING COLLECTIBLES</b>
-                    </Typography>
-                  </Box>
-                </Stack> */}
                 <Box
                   display="flex"
                   justifyContent="left"
                   alignItems="center"
                   sx={{
-                    fontFamily: "Futura", // Adicione a fonte aqui
+                    fontFamily: "Futura",
                   }}
+                  marginTop={10}
                 >
                   <img
                     src="/assets/logo-light.png"
@@ -311,7 +259,7 @@ export default function Footer() {
                           height: "10px",
                           marginRight: "10px",
                           justifyContent: "center",
-                          alignSelf: "center"
+                          alignSelf: "center",
                         }}
                       />
                       <img
