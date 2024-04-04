6fcd4912fef965ebde3d39bf4029631942ee5162
Gunichi <gunichi297@gmail.com>
2024-04-04 16:18:31 -0300

    new bg

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -49,7 +49,7 @@ export default function TopTrailer({ mediaType }: TopTrailerProps) {
                     height="100%"
                   /> */}
               <video
-                src="./assets/CheyniLoopBackground.mp4"
+                src="./assets/CheyniBg.mp4"
                 autoPlay
                 muted
                 loop
@@ -93,135 +93,6 @@ export default function TopTrailer({ mediaType }: TopTrailerProps) {
                   opacity: "45%", // Adjust opacity as needed
                 }}
               />
-              <Stack
-                direction="row"
-                spacing={1}
-                sx={{
-                  alignItems: "center",
-                  justifyContent: "center",
-                  position: "absolute",
-                  left: "50%",
-                  top: "20%",
-                  transform: "translate(-50%, -50%)",
-                  zIndex: 2,
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                  }}
-                >
-                  <img src="/assets/cLogo.png" alt="Cheyni" />
-                </Box>
-              </Stack>
-              <Stack
-                direction="row"
-                spacing={1}
-                sx={{
-                  alignItems: "center",
-                  justifyContent: "center",
-                  position: "absolute",
-                  left: "50%",
-                  top: "30%",
-                  transform: "translate(-50%, -50%)",
-                  zIndex: 2,
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    textAlign: "center",
-                  }}
-                >
-                  <IconButton>
-                    <img src="/assets/c3.png" alt="Cheyni" />
-                  </IconButton>
-                  <Typography variant="body2" color="white">
-                    Watch community-curated Films <br /> & collect membership
-                    NFTs
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    textAlign: "center",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      flexDirection: "row",
-                      textAlign: "center",
-                    }}
-                  >
-                    <IconButton>
-                      <img src="/assets/bitcoin.png" alt="Cheyni" />
-                    </IconButton>
-                    <IconButton>
-                      <img src="/assets/c1.png" alt="Cheyni" />
-                    </IconButton>
-                  </Box>
-                  <Typography variant="body2" color="white">
-                    Pay securely with <br /> Crypto & Credit Card
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    textAlign: "center",
-                    marginLeft: "50px",
-                  }}
-                >
-                  <IconButton>
-                    <img src="/assets/c3.png" alt="Cheyni" />
-                  </IconButton>
-                  <Typography variant="body2" color="white">
-                    Join exclusive communities & <br /> enjoy a premium journey
-                  </Typography>
-                </Box>
-              </Stack>
-              {/* <Stack
-                direction="row"
-                spacing={1}
-                sx={{
-                  alignItems: "center",
-                  justifyContent: "center",
-                  position: "absolute",
-                  left: "50%",
-                  top: "50%",
-                  transform: "translate(-50%, -50%)",
-                  zIndex: 2,
-                }}
-              >
-                <Button
-                  variant="contained"
-                  sx={{ 
-                    borderRadius: "50px", 
-                    padding: "10px 20px", 
-                    backgroundColor: isDarkMode ? "#FFF" : "#191761", 
-                    color: isDarkMode ? "#000" : "#FFF",
-                    "&:hover": {
-                      backgroundColor: isDarkMode ? "#191761" : "#FFF",
-                      color: isDarkMode ? "#FFF" : "#000",
-                    },
-                }}
-                > 
-                </Button>
-              </Stack> */}
             </Box>
           </>
         </Box>
